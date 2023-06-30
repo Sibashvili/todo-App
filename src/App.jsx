d8f18c52d06c5faaa0757b1b9693786d435705e7
Giorgi Sibashvili <124514809+Sib063@users.noreply.github.com>
2023-06-30 14:50:11 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import To from "./assets/TODO.png";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const [theme, setTheme] = useState("light");
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const handleInputChange = (event) => {
@@ -30,36 +30,91 @@ function App() {
       }
     }
   };
+  const handleRemoveTask = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+  const handleFilterClick = (filter) => {
+    setActiveFilter(filter);
+  };
+  const handleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+
+    console.log(theme);
+  };
   return (
-    <div className=" w-full">
-      <div className=" bg-mobile-light bg-no-repeat h-[200px] w-full  ">
-        <div className=" flex px-[26px] w-full justify-between x   ">
+    <div className={"w-full  "}>
+      <div
+        className={`  bg-no-repeat h-[200px] w-full ${
+          theme === "dark" ? "bg-mobile-dark" : "bg-mobile-light"
+        }  `}
+      >
+        <div className=" flex px-[26px] w-full justify-between x items-center  ">
           <img className=" w-[109px] h-[20px] mt-[48px]    " src={To} alt="" />
-          <img className=" mt-[48px]    " src={Moon} />
+          <div>
+            {theme === "dark" ? (
+              <div>
+                <img
+                  onClick={handleTheme}
+                  className=" mt-[48px]     "
+                  src={Moon}
+                />
+              </div>
+            ) : (
+              <div>
+                <img
+                  className=" mt-[48px]"
+                  onClick={handleTheme}
+                  src={Sun}
+                  alt=""
+                />
+              </div>
+            )}
+          </div>
         </div>
 
         <div className=" flex justify-center m-auto mt-[40px] bg-white w-[327px] h-[48px] rounded    items-center ">
-          <button className="w-[20px] h-[20px] rounded-full border-2 border-solid bg-transparent bg-opacity-100"></button>
+          <button className="w-[20px] h-[20px] rounded-full border-2 border-solid bg-transparent bg-opacity-100 ml-[10px]  "></button>
           <input
             value={inputValue}
             onChange={handleInputChange}
             onKeyDown={handleKeyPress}
-            className="  w-[290px] h-[48px]    "
+            className="  w-[290px] h-[48px] ml-[10px] border-none focus:outline-none font-normal   "
             type="text"
             placeholder="Create a new todo…"
           />
         </div>
       </div>
-      <div className=" p-[24px]">
-        {todos.map((todo, index) => (
-          <div
-            className="  h-[52px] bg-white border-b border-gray-300 h-1 bg-transparent mt-[] p-[24px] flex items-center --tw-shadow-custom: '0px 35px 50px -15px rgba(194, 195, 214, 0.50)' rounded "
-            key={index}
-          >
-            {todo.command}
+      {todos.length > 0 && (
+        <ul className=" mt-[-19px] text-fontColor   w-[327px] mx-auto block border-t-8 border-white   rounded-t-lg text-[12px] font-normal   ">
+          {todos.map((todo, index) => (
+            <li
+              className=" w-full    border-b   bg-white border-gray-300 h-1 bg-transparent  p-6 flex   items-center justify-between  "
+              key={index}
+            >
+              {todo.command}
+              <img
+                className=" cursor-pointer"
+                src={Cross}
+                alt=""
+                onClick={handleRemoveTask}
+              />
+            </li>
+          ))}
+          <div className="border-b rounded-b-lg p-[20px]  shadow-3xl bg-white border-gray-300 flex items-center justify-between ">
+            <span className=" text-customGrayishBlue">
+              {todos.length} items left
+            </span>
+            <button className="text-customGrayishBlue">Clear Comleted</button>
           </div>
-        ))}
-      </div>
+          <div className=" mt-[16px] w-[327px]  flex bg-white gap-6 h-[48px] items-center justify-center  ">
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+          </div>
+        </ul>
+      )}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,25 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "desktop-dark": "url('.src/assets/bg-desktop-dark.jpg')",
-        "desktop-light": "url('.src/assets/bg-desktop-light.jpg')",
-        "mobile-dark": "url('.src/assets/bg-mobile-dark.jpg')",
+        "desktop-dark": "url('/.src/assets/bg-desktop-dark.jpg')",
+        "desktop-light": "url('/.src/assets/bg-desktop-light.jpg')",
+        "mobile-dark": "url('./src/assets/bg-mobile-dark.jpg')",
         "mobile-light": "url('./src/assets/bg-mobile-light.jpg')",
         "todo-img": "url('./src/assets/TODO.jpg')",
       },
+      boxShadow: {
+        "3xl": "0px 35px 50px -15px rgba(194, 195, 214, 0.50);",
+      },
+      colors: {
+        customGrayishBlue: "#9495A5",
+      },
+      colorsback: {
+        customBack: "#FAFAFA",
+      },
+      colors: {
+        fontColor: "#494C6B",
+      },
+
       fontFamily: {
         "Josefin Sans": ["Josefin Sans", "sans-serif"],
       },
