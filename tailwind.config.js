de6ef782f25b109d3b3083098bed58c73b1b0d20
Giorgi Sibashvili <124514809+Sib063@users.noreply.github.com>
2023-07-05 19:04:27 +0400

    style:change background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "desktop-dark": "url('./src/assets/bg-desktop-dark.jpg')",
-        "desktop-light": "url('./src/assets/bg-desktop-light.jpg')",
-        "mobile-dark": "url('./src/assets/bg-mobile-dark.jpg')",
-        "mobile-light": "url('./src/assets/bg-mobile-light.jpg')",
-        "todo-img": "url('./src/assets/TODO.jpg')",
-      },
       boxShadow: {
         "3xl": "0px 35px 50px -15px rgba(194, 195, 214, 0.50);",
       },
